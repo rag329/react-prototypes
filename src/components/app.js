5a34a88b7b1b494e11f06ad522c96f5851e249ed
Matt Kirby <MattKir@aol.com>
2018-07-31 12:02:29 -0700

    Student Grade Table with React

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,31 @@
+import React from 'react';
+import Table from './table';
 
+function App(){
+    return (<div className='container'>
+                <h1>Student Grade Table</h1>
+                <Table data={students}/>
+            </div>
+    )
+}
+
+const students = [
+   {
+       student: 'Walter White',
+       course: 'Chemistry',
+       grade: '90'
+   },
+   {
+       student: 'Jesse Pinkman',
+       course: 'Cooking',
+       grade: '92.3'
+   },
+   {
+       student: 'Gus Fringe',
+       course: 'Business',
+       grade: '89'
+   }
+];
+
+export default App;
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/app';
 
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    <App/>,
     document.getElementById('root')
 );
+
+// function luckyNumber(){
+//     return Math.floor(Math.random() * 1001);
+// }
+
+// function greeting(user){
+//     user = {name, luckyNumber};
+//     const element = (<div className='container'>
+//                         <h1
+//                             >Welcome {user.name}
+//                         </h1>
+//                         <h2 className='text-muted'>
+//                             Your lucky number is: {user.luckyNumber}
+//                         </h2>
+//                     </div>
+//     );
+//     return element;
+// }
+
+// const superMan = [{
+//     name: 'Clark Kent', 
+//     luckyNumber: luckyNumber()
+// }];
